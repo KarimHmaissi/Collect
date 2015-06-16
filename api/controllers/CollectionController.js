49954f2cbd438d1d3ca934e352c6cbde0a90c128
karimhmaissi <karimhmaissi@gmail.com>
2015-06-16 16:56:26 +0100

    Collection controller

---
+++ api/controllers/CollectionController.js
@@ -0,0 +1,19 @@
+/**
+ * CollectionController
+ *
+ * @description :: Server-side logic for managing Collections
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+	restricted: function (req, res) {
+		return res.ok("hit collection/restricted")
+	},
+
+	open: function (req, res) {
+		return res.ok("hit collection/open");
+	}
+
+};
+
--- config/waterlock.js
+++ config/waterlock.js
@@ -56,7 +56,7 @@ module.exports.waterlock = {
   jsonWebTokens:{
 
     // CHANGE THIS SECRET
-    secret: 'this is my secret',
+    secret: 'Karim Hmaissi Oush',
     expiry:{
       unit: 'days',
       length: '7'
