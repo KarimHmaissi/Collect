ae6ff933c17df1206f245cff8f0068e0ca871e4e
karimhmaissi <karimhmaissi@gmail.com>
2015-06-26 09:53:09 +0100

    db

--- config/connections.js
+++ config/connections.js
@@ -43,7 +43,7 @@ module.exports.connections = {
   ***************************************************************************/
   postgresqlServer: {
     adapter: 'sails-postgresql',
-    host: '192.168.0.2',
+    host: '192.168.0.1',
     user: 'postgres',
     password: 'qpalzm12',
     database: 'collectApi'
