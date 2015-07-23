d05260ea63aaa4c51f935ddf7ddef13b9c7ab086
karimhmaissi <karimhmaissi@gmail.com>
2015-07-23 13:19:38 +0100

    adding links function

--- api/controllers/GroupController.js
+++ api/controllers/GroupController.js
@@ -28,59 +28,61 @@
  		if(!CollectionUtility.validateGroup(group) || !req.session.user) {
  			sails.log("error validating group or no session");
  			res.json({err: "Error adding link"});
+ 		} else {
+	 		//if collection id is valid
+	 		var collectionTestHandler = function (collections) {
+	 			if(collections.length > 0) {
+	 				// collection exists
+	 				group.memberOf = meta.collectionId;
+	 				sails.log("found collection continuing");
+					addLinkToGroup();
+	 			} else {
+	 				// collection does not exist
+	 				sails.log("cant find collection invalid id");
+	 				res.json({err: "Collection does not exist"});
+	 			}
+	 		};
+
+	 		Collection.find().where({id: meta.collectionId}).then(collectionTestHandler);
+
+
+	 		var addLinkToGroup = function () {
+
+	 			var groupTestHandler = function (groups) {
+	 				//if group exists in collection
+	 				if(groups.length > 0) {
+	 					// group exists
+	 					groups[0].ownedLinks = groups[0].ownedLinks.concat(group.ownedLinks);
+	 					groups[0].save(function (err, finishedGroup) {
+	 						if(!err) {
+	 							sails.log("saved group");
+		 						res.json(finishedGroup);
+	 						} else {
+	 							sails.log("error saving group");
+	 							res.json(err);
+	 						}
+	 					});
+	 				} else {
+	 					//group does not exist
+	 					Group.create(group).exec(function (err, savedGroup) {
+	 						if(!err) {
+	 							sails.log("created new group");
+	 							res.json(err);
+	 						} else {
+	 							sails.log("eroor creating new group");
+	 							res.json(savedGroup);
+	 						}
+	 						
+
+	 					});
+	 				}
+	 			};
+	 			
+	 			Group.find().where({name: group.name, memberOf: group.memberOf}).then(groupTestHandler);	
  		}
 
 
- 		//if collection id is valid
- 		var collectionTestHandler = function (collections) {
- 			if(collections.length > 0) {
- 				// collection exists
- 				group.memberOf = meta.collectionId;
- 				sails.log("found collection continuing");
-				addLinkToGroup();
- 			} else {
- 				// collection does not exist
- 				sails.log("cant find collection invalid id");
- 				res.json({err: "Collection does not exist"});
- 			}
- 		};
-
- 		Collection.find().where({id: meta.collectionId}).then(collectionTestHandler);
-
-
- 		var addLinkToGroup = function () {
-
- 			var groupTestHandler = function (groups) {
- 				//if group exists in collection
- 				if(groups.length > 0) {
- 					// group exists
- 					groups[0].ownedLinks = groups[0].ownedLinks.concat(group.ownedLinks);
- 					groups[0].save(function (err, finishedGroup) {
- 						if(!err) {
- 							sails.log("saved group");
-	 						res.json(finishedGroup);
- 						} else {
- 							sails.log("error saving group");
- 							res.json(err);
- 						}
- 					});
- 				} else {
- 					//group does not exist
- 					Group.create(group).exec(function (err, savedGroup) {
- 						if(!err) {
- 							sails.log("created new group");
- 							res.json(err);
- 						} else {
- 							sails.log("eroor creating new group");
- 							res.json(savedGroup);
- 						}
- 						
-
- 					});
- 				}
- 			};
- 			
- 			Group.find().where({name: group.name, memberOf: group.memberOf}).then(groupTestHandler);	
+ 		
  		};
  	},
 
